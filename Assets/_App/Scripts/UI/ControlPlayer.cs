5e99b2487d9a6f7357494291797d61518d804916
costudiolabor <costudiolabor@gmail.com>
2026-06-26 11:49:01 +0300

    refactor: optimizing the ControlPlayer

--- Assets/_App/Scripts/UI/ControlPlayer.cs
+++ Assets/_App/Scripts/UI/ControlPlayer.cs
@@ -1,29 +1,38 @@
+using System;
 using UnityEngine;
+
 
 public class ControlPlayer : MonoBehaviour
 {
     [SerializeField] private RectTransform uiElement;
     [SerializeField] private float groundLevel = 0f;
     [SerializeField] private float smoothSpeed = 5f;
-    public Player Player3D { get; set; }
+    public Transform Player3D { get; set; }
     public Camera CameraMain { get; set; }
     private Vector3 _targetPosition;
-
+    private Vector3 _uiWorldPosition;
+    private Plane _groundPlane;
+    private Ray _ray;
+    private Vector3 _worldPoint;
+  
+    public void Initialize()
+    {
+        _groundPlane = new Plane(Vector3.up, new Vector3(0, groundLevel, 0));
+    }
     private void Update()
     {
         if (uiElement == null || Player3D == null) return;
-        Vector3 uiWorldPosition = uiElement.position;
-        Ray ray = CameraMain.ScreenPointToRay(uiWorldPosition);
-        Plane groundPlane = new Plane(Vector3.up, new Vector3(0, groundLevel, 0));
-        if (groundPlane.Raycast(ray, out float distance))
+        _uiWorldPosition = uiElement.position;
+        _ray = CameraMain.ScreenPointToRay(_uiWorldPosition);
+        if (_groundPlane.Raycast(_ray, out float distance))
         {
-            Vector3 worldPoint = ray.GetPoint(distance);
-            _targetPosition = new Vector3(
-                worldPoint.x, 
-                Player3D.transform.position.y,
-                worldPoint.z
-            );
+            _worldPoint = _ray.GetPoint(distance);
+            _targetPosition.x = _worldPoint.x;
+            _targetPosition.y = Player3D.position.y;
+            _targetPosition.z = _worldPoint.z;
         }
-        Player3D.transform.position = Vector3.Lerp(Player3D.transform.position, _targetPosition, Time.deltaTime * smoothSpeed);
+        
+        Player3D.position = Vector3.Lerp(Player3D.position, _targetPosition, Time.deltaTime * smoothSpeed);
     }
+    
 }
--- Assets/_App/Scripts/UI/PlayerUI.cs
+++ Assets/_App/Scripts/UI/PlayerUI.cs
@@ -9,11 +9,14 @@ public class PlayerUI : MonoBehaviour
     [SerializeField] private ControlPlayer controlPlayer;
     private Vector2 _targetPosition;
 
-    public void Set3DPlayer(Player player3D) => controlPlayer.Player3D = player3D;
+    private void Awake() => Initialize();
+    public void Initialize() => controlPlayer.Initialize();
+    public void Set3DPlayer(Player player3D) => controlPlayer.Player3D = player3D.transform;
     public void SetCamera(Camera cameraMain) => controlPlayer.CameraMain = cameraMain; 
     private void Update() 
     {
-        rectTransform.position = Vector2.Lerp(rectTransform.position, _targetPosition, Time.deltaTime * smoothSpeed);
+        rectTransform.position = Vector2.Lerp(rectTransform.position, _targetPosition, 
+            Time.deltaTime * smoothSpeed);
     }
     public void SetNewTargetPosition(Vector2 newPosition) => _targetPosition = newPosition;
     public void SetPosition(Vector2 position) => rectTransform.position = position;
